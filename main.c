c4394752fb176bbb24f65c5459ef971a266406e9
senpairo111 <nadavh2611@gmail.com>
2023-11-02 19:02:28 +0200

    implemented apple

--- main.c
+++ main.c
@@ -8,196 +8,262 @@
 
 #define DELTA_TIME_MILIS ((float)(SDL_GetTicks() - start))
 
-#define MAX(a,b) (((a) > (b)) ? (a) : (b))
-#define MIN(a,b) (((a) < (b)) ? (a) : (b))
-#define RANGE(a, b) (a <= b)//(ABS(a) <= ABS(b)) 
-#define SIGN(x) ((x > 0) - (x < 0)) 
-#define ABS(x) ((signed int)x)*(sign((signed int)x))
-#define MOD(a,b) ((a%b)+b)%b
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+#define RANGE(a, b) (a <= b) //(ABS(a) <= ABS(b))
+#define SIGN(x) ((x > 0) - (x < 0))
+#define ABS(x) ((signed int)x) * (sign((signed int)x))
+#define MOD(a, b) (((a % b) + b) % b)
+#define RAND_RANGE(upper, lower)  (MOD(((unsigned short)rand()), ((unsigned short)upper-(unsigned short)lower)) + (unsigned short)lower) // ((rand() % (upper - lower + 1)) + lower)
+
 
 #define CLEAN_FACE(a) a & 3
 
-typedef struct {
-    signed short int x;
-    signed short int y;
-    
-} vec2_int;
+typedef struct
+{
+    unsigned short x;
+    unsigned short y;
 
+} vec2_short;
 
-typedef union { 
+typedef union
+{
     unsigned char as_char : 2;
-    struct {
+    struct
+    {
         bool up : 1;
         bool down : 1;
-    }; 
-} face; //[up, down]  -  [1,0]: up = 1, [0,1]: down = 2, [1,1]: left = 3, [0,0]: right = 0,  |   right=~left, up=~down 
+    };
+} face; //[up, down]  -  [1,0]: up = 1, [0,1]: down = 2, [1,1]: left = 3, [0,0]: right = 0,  |   right=~left, up=~down
 
 typedef struct snake_segment
 {
-   vec2_int location;
-   face facing;
+    vec2_short location;
+    face facing;
 } snake_segment;
 
 bool not_lost = true;
 unsigned int start;
-node * p_snake_head; // snake is linked list of snake_segment's
+node *p_snake_head; // snake is linked list of snake_segment's
+vec2_short apple = {
+    .x = 0,
+    .y = 0
+};
 
 SDL_Event window_event;
 SDL_Window *win;
-SDL_Renderer * renderer;
+SDL_Renderer *renderer;
 
-
-vec2_int facing_to_vec(face facing){
-    vec2_int delta = {
+vec2_short facing_to_vec(face facing)
+{
+    vec2_short delta = {
         .x = 0,
-        .y = 0
-    };
-    delta.y -= (SEGMENT_SPACING*((facing.up) && (!facing.down))); // up
-    delta.y += (SEGMENT_SPACING*((!facing.up) && (facing.down))); // down
-    delta.x += (SEGMENT_SPACING*((!facing.up) && (!facing.down))); // right
-    delta.x -= (SEGMENT_SPACING*((facing.up) && (facing.down))); // left
+        .y = 0};
+    delta.y -= (SEGMENT_SPACING * ((facing.up) && (!facing.down)));  // up
+    delta.y += (SEGMENT_SPACING * ((!facing.up) && (facing.down)));  // down
+    delta.x += (SEGMENT_SPACING * ((!facing.up) && (!facing.down))); // right
+    delta.x -= (SEGMENT_SPACING * ((facing.up) && (facing.down)));   // left
     return delta;
 }
+vec2_short grid_pos_to_screen_pos(vec2_short grid_pos){
+    vec2_short screen_pos = {
+        .x = grid_pos.x * SEGMENT_SPACING,
+        .y = grid_pos.y * SEGMENT_SPACING
+    };
+    return screen_pos;
+}
+vec2_short screen_pos_to_grid_pos(vec2_short screen_pos){
+    vec2_short grid_pos = {
+        .x = screen_pos.x / SEGMENT_SPACING,
+        .y = screen_pos.y / SEGMENT_SPACING
+    };
+    return grid_pos;
+}
+void spawn_new_apple() {
+    vec2_short * full_grid[GRID_WIDTH][GRID_HEIGHT];
+    vec2_short * grid_as_1d = full_grid;
+    node * p_cur = p_snake_head;
+    unsigned short snake_length = 0;
 
-void delete_end(){
-    snake_segment * p_segment = (snake_segment (*))remove_last(p_snake_head);
+    for (unsigned int i = 0; i < GRID_HEIGHT*GRID_WIDTH; i++){
+        full_grid[(i % GRID_WIDTH)][(i / GRID_WIDTH)] = NULL;
+    }
+    // putts all taken snake segments in the begining of the grid 
+    while (p_cur != NULL)
+    {
+        vec2_short grid_pos = screen_pos_to_grid_pos(((snake_segment *)p_cur->p_data)->location);
+        // *(grid_as_1d + snake_length) = grid_pos;
+        full_grid[grid_pos.y][grid_pos.x] = &((vec2_short){.x = snake_length % GRID_WIDTH,
+                                                        .y = snake_length / GRID_WIDTH});
+        snake_length++;
+        p_cur = p_cur->p_next;
+    }
+
+    short random_in_range = RAND_RANGE(snake_length, GRID_HEIGHT*GRID_WIDTH);
+    // printf("%i\n", (full_grid[(random_in_range % GRID_WIDTH)][(random_in_range / GRID_WIDTH)]));
+    if ((full_grid[(random_in_range % GRID_WIDTH)][(random_in_range / GRID_WIDTH)]) == NULL) {
+        apple.x = (random_in_range % GRID_WIDTH) * SEGMENT_SPACING;
+        apple.y = (random_in_range / GRID_WIDTH) * SEGMENT_SPACING;
+    }
+    else{
+        apple.x = (grid_as_1d + random_in_range)->x;
+        apple.y = (grid_as_1d + random_in_range)->y;
+    }
+}
+
+
+void delete_end()
+{
+    snake_segment *p_segment = (snake_segment(*))remove_last(p_snake_head);
     free(p_segment);
 }
 
-void check_colisions(){
-    snake_segment * p_head_segment = (snake_segment *)p_snake_head->p_data;
+void check_colisions()
+{
+    snake_segment *p_head_segment = (snake_segment *)p_snake_head->p_data;
     // if the snake exited the screen
-    if (! (RANGE(p_head_segment->location.y, HEIGHT) && RANGE(p_head_segment->location.x, WIDTH)))
+    if (!(RANGE(p_head_segment->location.y, HEIGHT) && RANGE(p_head_segment->location.x, WIDTH)))
     {
-        
+
         // p_head_segment->location.x = WIDTH/2; // for debug
         // p_head_segment->location.y = HEIGHT/2;// for debug
 
         not_lost = false; // death
-    } else {
+    }
+    else
+    {
         // check self collision
-        node * p_cur = p_snake_head->p_next;
+        node *p_cur = p_snake_head->p_next;
         // printf("(%i, %i) -> ", p_head_segment->location.x, p_head_segment->location.y); // TEMP
         while (p_cur != NULL)
         {
-            vec2_int cur_location = ((snake_segment *)p_cur->p_data)->location;
+            vec2_short cur_location = ((snake_segment *)p_cur->p_data)->location;
             // printf("(%i, %i) -> ", cur_location.x, cur_location.y); // TEMP
-            if (!memcmp(&cur_location, &p_head_segment->location, sizeof(vec2_int))){
+            if (!memcmp(&cur_location, &p_head_segment->location, sizeof(vec2_short)))
+            {
                 not_lost = false; // death
                 // printf("death");
                 break;
             }
             p_cur = p_cur->p_next;
         }
-        // printf("\n\n\n"); // TEMP 
-        
+        // printf("\n\n\n"); // TEMP
     }
 }
 
-void update_game(float dt){
-    
-    // add a new head 
-    node * p_new_head = malloc(sizeof(node));
+void update_game(float dt)
+{
+
+    // add a new head
+    node *p_new_head = malloc(sizeof(node));
     p_new_head->p_next = p_snake_head;
-    const snake_segment * p_old_segment = (snake_segment *)(p_snake_head->p_data); // old segment is used to check where to put new head
-    p_snake_head = p_new_head; 
-    snake_segment * p_new_segment = malloc(sizeof(snake_segment));
+    const snake_segment *p_old_segment = (snake_segment *)(p_snake_head->p_data); // old segment is used to check where to put new head
+    p_snake_head = p_new_head;
+    snake_segment *p_new_segment = malloc(sizeof(snake_segment));
     // memcpy(p_new_segment, p_old_segment, sizeof(snake_segment));
     p_new_segment->location = p_old_segment->location;
-    
+
     p_new_head->p_data = p_new_segment; // update the new head segment to the correct pointer
-    
+
     // printf(up_input ? "true\n" : "false\n");
-    
-    
+
     // if up ^ down --> direction is vertical, else direction is horizontal
     bool last_was_vertical = p_old_segment->facing.up ^ p_old_segment->facing.down;
-    // ignore the input in both the direction old_facing and ~old_facing to make it so input in the direction the 
+    // ignore the input in both the direction old_facing and ~old_facing to make it so input in the direction the
     //  snake is already headed in and input back to where you came from is not counted as meaningfull input and get ignored
-    up_input = up_input && !last_was_vertical; // ignore up input if last was vertical
-    down_input = down_input && !last_was_vertical; // ignore down input if last was vertical
+    up_input = up_input && !last_was_vertical;      // ignore up input if last was vertical
+    down_input = down_input && !last_was_vertical;  // ignore down input if last was vertical
     right_input = right_input && last_was_vertical; // ignore right input if last was horizontal
-    left_input = left_input && last_was_vertical; // ignore left input if last was horizontal
+    left_input = left_input && last_was_vertical;   // ignore left input if last was horizontal
 
     bool was_meaningful_input = up_input || down_input || right_input || left_input;
 
     // if (up=down=left=right=0) --> new_facing = old_facing
-    p_new_segment->facing.up   = (up_input || left_input) || // if meaningful input in one of them value needs to be true
-        ((!was_meaningful_input) && p_old_segment->facing.up); // if there wasnt any meaningful input put whatever direction the snake was going last
-    p_new_segment->facing.down = (down_input || (!right_input && left_input)) ||// if meaningful input in down value needs to be true and if in right false
-        ((!was_meaningful_input) && p_old_segment->facing.down); // if there wasnt any meaningful input put whatever direction the snake was going last
+    p_new_segment->facing.up = (up_input || left_input) ||                                // if meaningful input in one of them value needs to be true
+                               ((!was_meaningful_input) && p_old_segment->facing.up);     // if there wasnt any meaningful input put whatever direction the snake was going last
+    p_new_segment->facing.down = (down_input || (!right_input && left_input)) ||          // if meaningful input in down value needs to be true and if in right false
+                                 ((!was_meaningful_input) && p_old_segment->facing.down); // if there wasnt any meaningful input put whatever direction the snake was going last
     // printf(was_meaningful_input ? "true\n" : "false\n");
     // printf("%i\n", CLEAN_FACE(p_new_segment->facing.as_char));
 
     // move head
-    vec2_int delta = facing_to_vec(p_new_segment->facing);
+    vec2_short delta = facing_to_vec(p_new_segment->facing);
     p_new_segment->location.y += delta.y;
     p_new_segment->location.x += delta.x;
-    
+
     // remove tail
-    delete_end();// TODO: make it conditional to if eaten apple
-    
+    if (!((p_new_segment->location.y == apple.y) && (p_new_segment->location.x == apple.x)))
+    {
+        delete_end(); // TODO: make it conditional to if eaten apple
+    }
+    else {
+        spawn_new_apple();
+    }
+   
+
     check_colisions();
-    
 }
-
-
 
 void draw()
 {
     SDL_RenderClear(renderer);
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-    
+
     SDL_RenderFillRect(renderer, NULL);
 
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
-    node * p_cur = p_snake_head;
+    node *p_cur = p_snake_head;
     SDL_Rect segment_rect = {
-        .h = 2*SEGMENT_RADIUS+1,
-        .w = 2*SEGMENT_RADIUS+1,
+        .h = 2 * SEGMENT_RADIUS + 1,
+        .w = 2 * SEGMENT_RADIUS + 1,
         .x = 0,
-        .y = 0
-    };
+        .y = 0};
 
     SDL_Rect connection_rect = {
         .h = SEGMENT_SPACING,
         .w = SEGMENT_SPACING,
         .x = 0,
-        .y = 0
-    };
+        .y = 0};
     // draws rest of snake segments and connects them
     while (p_cur->p_next != NULL)
-    {   
-        snake_segment * p_segment = ((snake_segment *)(p_cur->p_data));
+    {
+        snake_segment *p_segment = ((snake_segment *)(p_cur->p_data));
 
         // draw the segment
-        segment_rect.x = p_segment->location.x - SEGMENT_RADIUS; 
+        segment_rect.x = p_segment->location.x - SEGMENT_RADIUS;
         segment_rect.y = p_segment->location.y - SEGMENT_RADIUS;
         SDL_RenderFillRect(renderer, &(segment_rect));
 
         // draw connections
         face connection_facing = p_segment->facing;
         connection_facing.as_char = ~connection_facing.as_char;
-        vec2_int delta = facing_to_vec(connection_facing);
+        vec2_short delta = facing_to_vec(connection_facing);
         connection_rect.x = segment_rect.x;
-        connection_rect.y = segment_rect.y; 
-        connection_rect.h = ((signed short)delta.y) + ((2*SEGMENT_RADIUS+1));
-        connection_rect.w = ((signed short)delta.x) + ((2*SEGMENT_RADIUS+1));
+        connection_rect.y = segment_rect.y;
+        connection_rect.h = ((signed short)delta.y) + ((2 * SEGMENT_RADIUS + 1));
+        connection_rect.w = ((signed short)delta.x) + ((2 * SEGMENT_RADIUS + 1));
         // printf("\n%i\n", SIGN((signed short) delta.x));
         // printf("%i\n", ((signed short)(2*SEGMENT_RADIUS+1))*SIGN((signed short) delta.x));
 
         SDL_RenderFillRect(renderer, &connection_rect);
 
         p_cur = p_cur->p_next;
-        
     }
     // draw last segment indpendently to not draw its connection
-    snake_segment * p_segment = ((snake_segment *)(p_cur->p_data));
-    segment_rect.x = p_segment->location.x - SEGMENT_RADIUS; 
+    snake_segment *p_segment = ((snake_segment *)(p_cur->p_data));
+    segment_rect.x = p_segment->location.x - SEGMENT_RADIUS;
     segment_rect.y = p_segment->location.y - SEGMENT_RADIUS;
     SDL_RenderFillRect(renderer, &(segment_rect));
 
-    
+    // draw apple
+    SDL_Rect apple_rect = {
+        .x = apple.x - APPLE_RADIUS,
+        .y = apple.y - APPLE_RADIUS,
+        .w = 2*APPLE_RADIUS + 1,
+        .h = 2*APPLE_RADIUS + 1
+    };
+    SDL_RenderFillRect(renderer, &(apple_rect));
+
     // // for (int i = 0; i < main_window.width; i++){
     // //     for (int j = 0; j < main_window.height; j++){
     // //         SDL_RenderDrawPoint(renderer, i, j);
@@ -213,7 +279,7 @@ void draw()
     // };
 
     // SDL_RenderFillRect(renderer, &b);
-    
+
     // // for (int i = main_game.b.y + ((main_window.height - BALL_WIDTH)*0.5) ;
     // //              i < main_game.b.y + ((main_window.height + BALL_WIDTH)*0.5); i++){
     // //     for (int j = main_game.b.x + ((main_window.width - BALL_WIDTH)*0.5);
@@ -242,34 +308,34 @@ void draw()
     SDL_RenderPresent(renderer);
 }
 
-
-void init() {
+void init()
+{
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, 0, &win, &renderer);
 
     SDL_RenderSetScale(renderer, 1, 1);
-    
+
     p_snake_head = malloc(sizeof(node));
-    p_snake_head->p_next=NULL;
-    snake_segment * p_head_segment = malloc(sizeof(snake_segment));
+    p_snake_head->p_next = NULL;
+    snake_segment *p_head_segment = malloc(sizeof(snake_segment));
     p_head_segment->facing = (face){
         .up = 0,
-        .down = 0
-    };
-    p_head_segment->location = (vec2_int){
-        .x = WIDTH/2,
-        .y = HEIGHT/2};
+        .down = 0};
+    p_head_segment->location = grid_pos_to_screen_pos((vec2_short){
+        .x = GRID_WIDTH / 2,
+        .y = GRID_HEIGHT / 2});
     p_snake_head->p_data = p_head_segment;
 
-    start = SDL_GetTicks();
+    spawn_new_apple();
 
-    
+    start = SDL_GetTicks();
 }
 void update(float dt)
 {
     if (SDL_PollEvent(&window_event))
     {
-        if (window_event.type == SDL_QUIT){
+        if (window_event.type == SDL_QUIT)
+        {
             not_lost = false;
         }
     }
@@ -281,26 +347,26 @@ void update(float dt)
 // int SDLMAIN_DECLSPEC SDL_main(int argc, char *argv[])
 int main(int argc, char *argv[])
 {
-    
+
     char title[] = "snake";
     init();
     // free(p_snake_head);
-    
-    SDL_SetWindowTitle(win, (char (*))&title);
- 
+
+    SDL_SetWindowTitle(win, (char(*)) & title);
+
     while (not_lost)
     {
-        
+
         check_press(&window_event);
         float dtime = DELTA_TIME_MILIS;
-        if (dtime >= (1000.0/FRAME_RATE)){
-            
+        if (dtime >= (1000.0 / FRAME_RATE))
+        {
+
             update(dtime);
 
-            sprintf((char (*))&title, "snake    fps: %f\n", (1000.0/dtime));
-            SDL_SetWindowTitle(win, (char (*))&title);
+            sprintf((char(*)) & title, "snake    fps: %f\n", (1000.0 / dtime));
+            SDL_SetWindowTitle(win, (char(*)) & title);
         }
-        
     }
     SDL_DestroyWindow(win);
     SDL_QuitSubSystem(SDL_INIT_EVERYTHING);
